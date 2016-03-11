5e8809f6a04e67f210e9fc320a07fe3a4c19fc7b
zwpdbh <hyperion_z@outlook.com>
2016-03-11 14:46:30 +1300

    < functionality is finished for lab 03 > add more method for extension

--- 03/RecursiveApp.java
+++ 03/RecursiveApp.java
@@ -1,8 +1,8 @@
+//package week03;
 /**
  * Created by zw on 3/11/16.
  * @author zw
  */
-
 
 public class RecursiveApp {
     public static void main(String[] args) {
@@ -11,11 +11,22 @@ public class RecursiveApp {
 
         System.out.println(digit(-1234567));
         System.out.println(sumOfDigit(-523));
+        
+        Tower t = new Tower();
+        t = t.add('a');
+        t = t.add('b');
+        t = t.add('c');
+        t = t.add('b');
+        
+        System.out.println("The number of block of b is : " + t.count('b'));
+        System.out.println("The height of block of tower is: " + t.height());
+        System.out.println(t);
+
     }
 
     /**
      * @param n
-     * @return long
+     * @return long , represent the length of the digital number
      */
     public static long digit(long n) {
         if (n < 0) {
@@ -31,7 +42,7 @@ public class RecursiveApp {
 
     /**
      * @param n
-     * @return long
+     * @return long , represent the sum of digital number, such as"123" = 6
      */
     public static long sumOfDigit(long n) {
         if (n < 0) {
---
+++ 03/Tower.java
@@ -0,0 +1,91 @@
+//package week03;
+
+/**
+ *  A recursive representation of a tower of blocks.
+ *
+ * @author Michael Albert
+ */
+public class Tower{
+
+    /** The top block. */
+    private char top;
+    
+    /** The rest of the tower. */
+    private Tower rest;
+
+    /**
+     * Creates a new empty Tower.
+     */
+    public Tower() {
+        this.top = ' ';
+        this.rest = null;
+    }
+    
+    /**
+     *  External classes can only create empty towers and manipulate
+     *  them using public methods, because this constructor is
+     *  private.
+     * @param top the top block in this tower
+     * @param rest the rest of the tower
+     */
+    private Tower(char top, Tower rest) {
+        this.top = top;
+        this.rest = rest;
+    }
+
+    /**
+     *  Returns true if this tower is empty, otherwise false.  Empty
+     *  towers are represented with the top block being a space
+     *  character.
+     * @return whether the tower is empty or not.
+     */
+    public boolean isEmpty() {
+
+        return top == ' ';
+    }
+        
+    /**
+     *  Creates a new tower by adding the given block to the top of
+     *  this tower.
+     * @param block a block to add to the top of this tower.
+     * @return a new tower created by adding a block to the top of
+     * this tower.
+     */
+    public Tower add(char block) {
+        return new Tower(block, this);
+    }
+
+    public int height() {
+        int sum = 0;
+        Tower currentNode = this;
+        while (currentNode.rest != null) {
+            sum++;
+            currentNode = currentNode.rest;
+        }
+        
+        return sum;
+    }
+    
+    public int count(char c) {
+        int sum = 0;
+        Tower currentNode = this;
+        while (currentNode.rest != null) {
+            if (currentNode.top == c) {
+                sum ++;
+            }
+            currentNode = currentNode.rest;
+        }
+        return sum;
+    }
+    
+    public String toString() {
+        String str = "";
+        Tower currentNode = this;
+        while (currentNode.rest != null) {
+            str += currentNode.top;
+            currentNode = currentNode.rest;
+        }
+        
+        return "Tower: " + str;
+    }
+}
