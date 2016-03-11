d70d5a58f7a558bbae630145ce37ee40d203b200
zwpdbh <hyperion_z@outlook.com>
2016-03-11 13:26:45 +1300

    no message

--- 03/src/RecursiveApp.java
+++ 03/src/RecursiveApp.java
@@ -6,14 +6,33 @@
 
 public class RecursiveApp {
     public static void main(String[] args) {
-        System.out.println(digit(111));
+        System.out.println(digit(123456));
+        System.out.println(sumOfDigit(123));
     }
 
+    /**
+     * @param n
+     * @return long
+     */
     public static long digit(long n) {
         if (n < 10) {
             return 1;
         } else {
             return 1 + digit(n/10);
         }
+    }
+
+    /**
+     * @param n
+     * @return long
+     */
+    public static long sumOfDigit(long n) {
+        int sum = 0;
+
+        if (n<10) {
+            return n;
+        }
+
+        return sum;
     }
 }
