5e5313ca98fd67c88fb6516d1197aae7656fa449
zwpdbh <hyperion_z@outlook.com>
2016-03-28 16:42:27 +1300

    < rowValuesIncrease, task 1 finished. > task 2

--- week05/src/TableauApp.java
+++ week05/src/TableauApp.java
@@ -24,9 +24,12 @@ public class TableauApp {
         // Test for get number of rows in a specific columns.
         //getNumberOfRow(valid, 1);
         // Test for method: rowLengthsDecrease
-        int [][] invalid = {{1, 2, 3}, {7}, {1, 2, 3, 4},{3, 9, 12},};
+        int [][] invalid1 = {{1, 2, 3}, {7}, {1, 2, 3, 4},{3, 9, 12},};
         //getNumberOfRow(invalid, 3);
-        System.out.println(rowLengthsDecrease(invalid));
+        System.out.println(rowLengthsDecrease(invalid1));
+
+        int[][] invalid2 = {{1, 4, 5, 10, 11}, {2, 6, 8}, {7, 9, 12}, {3}};
+        System.out.println("The result of rowValuesIncrease is: " + rowValuesIncrease(invalid2));
     }
 
     /**
@@ -64,7 +67,7 @@ public class TableauApp {
      * A method that returns true if no row is longer than a preceding row, otherwise false.
      * @param t a two-dimensional array which represent a tableau.
      *
-     * @return true if the parameter is a valid tableau, otherwise false.
+     * @return returns true if no row is longer than a preceding row, otherwise false.
      * */
     public static boolean rowLengthsDecrease(int[][] t) {
         int previousNumberOfRow = getNumberOfRow(t, 0);
@@ -82,6 +85,9 @@ public class TableauApp {
 
     /**
      * A helper function which return the number of rows in a specific columns in a two dimensional array.
+     * @param t a two-dimensional array.
+     * @param indexCol the specific column.
+     * @return int which is the number of rows in the specific column.
      * */
     private static int getNumberOfRow(int[][] t, int indexCol) {
         int maxExpectedRows = t.length;
@@ -93,8 +99,32 @@ public class TableauApp {
                 break;
             }
         }
-        System.out.println("The number of rows in column: " + indexCol + " is: " + i);
+        // System.out.println("The number of rows in column: " + indexCol + " is: " + i);
         return i;
+    }
+
+
+    /**
+     * A method that returns true if from left to right in any row, the integers are increasing, otherwise false.
+     * @param t a two-dimensional array.
+     * @return returns true if from left to right in any row, the integers are increasing, otherwise false.
+     * */
+    public static boolean rowValuesIncrease(int[][] t) {
+        int totalColumn = t[0].length; // assume the first row has the max column.
+        for (int col=0; col<totalColumn; col++) {
+            for (int row=0; row<getNumberOfRow(t, col)-1; row++) {
+                try {
+                    int current = t[row][col];
+                    int next = t[row+1][col];
+                    if (current>next) {
+                        return false;
+                    }
+                } catch (ArrayIndexOutOfBoundsException ex) {
+                    System.out.println(ex);
+                }
+            }
+        }
+        return true;
     }
 
 }
