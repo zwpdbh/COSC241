db465b97d25c400c020023e1d3feae466a15af00
zwpdbh <hyperion_z@outlook.com>
2016-03-28 20:39:47 +1300

    < week05 finished. > week06.

--- week05/src/TableauApp.java
+++ week05/src/TableauApp.java
@@ -3,7 +3,8 @@
  */
 package week05;
 
-import org.omg.CORBA.DynAnyPackage.Invalid;
+import java.util.HashSet;
+import java.util.Set;
 
 /**
  * Skeleton code for an array based implementation of Young's tableau.
@@ -18,36 +19,34 @@ public class TableauApp {
      * @param args command line arguments are not used.
      */
     public static void main(String[] args) {
-        int[][] valid = {{1, 4, 5, 10, 11}, {2, 6, 8}, {3, 9, 12}, {7}};
-        System.out.println(TableauApp.toString(valid));
 
-        // Test for get number of rows in a specific columns.
-        //getNumberOfRow(valid, 1);
-        // Test for method: rowLengthsDecrease
-        int [][] invalid1 = {{1, 2, 3}, {7}, {1, 2, 3, 4},{3, 9, 12},};
-        //getNumberOfRow(invalid, 3);
-        System.out.println(rowLengthsDecrease(invalid1));
+        // int[][] valid = {{1, 4, 5, 10, 11}, {2, 6, 8}, {3, 9, 12}, {7}};
+        // System.out.println(TableauApp.toString(valid));
 
-        int[][] invalid2 = {{1, 4, 5, 10, 11}, {2, 6, 8}, {7, 9, 12}, {3}};
-        System.out.println("The result of rowValuesIncrease is: " + rowValuesIncrease(invalid2));
+        int[][] invalid ={{1, 4, 5, 10, 11}, {2, 6, 8}, {3, 9, 12}, {7}};
+        System.out.println(TableauApp.toString(invalid));
+        System.out.println(rowValuesIncrease(invalid));
     }
 
     /**
      * Determines whether the array passed to it is a valid tableau or not.
      *
      * @param t a two-dimensional array to test for tableau-ness.
-     *
      * @return true if the parameter is a valid tableau, otherwise false
      */
-    public static boolean isTableau(int[][] t){
+    public static boolean isTableau(int[][] t) {
+        if (isSetOf1toN(t) && rowLengthsDecrease(t) &&
+                rowValuesIncrease(t) && columnValuesIncrease(t)) {
+            return true;
+        }
+
         return false;
     }
 
     /**
-     *  Returns a string representation of an array based tableau.
+     * Returns a string representation of an array based tableau.
      *
      * @param t a two-dimensional array which represents a tableau.
-     *
      * @return a string representation of an array based tableau.
      */
     public static String toString(int[][] t) {
@@ -56,7 +55,7 @@ public class TableauApp {
             for (int j = 0; j < t[i].length; j++) {
                 result.append(String.format("%-4s", t[i][j]));
             }
-            if (i < t.length-1) {
+            if (i < t.length - 1) {
                 result.append("\n");
             }
         }
@@ -64,16 +63,17 @@ public class TableauApp {
     }
 
     /**
-     * A method that returns true if no row is longer than a preceding row, otherwise false.
-     * @param t a two-dimensional array which represent a tableau.
+     * A method that returns true if no row is longer than a preceding
+     * row, otherwise false.
      *
-     * @return returns true if no row is longer than a preceding row, otherwise false.
-     * */
+     * @param t a two-dimensional array which represent a tableau.
+     * @return returns true if no row is longer than a preceding row.
+     */
     public static boolean rowLengthsDecrease(int[][] t) {
         int previousNumberOfRow = getNumberOfRow(t, 0);
-        for (int i=1; i<t.length; i++) {
+        for (int i = 1; i < t.length; i++) {
             int currentNumberOfRow = getNumberOfRow(t, i);
-            if (currentNumberOfRow>previousNumberOfRow) {
+            if (currentNumberOfRow > previousNumberOfRow) {
                 return false;
             } else {
                 previousNumberOfRow = currentNumberOfRow;
@@ -84,39 +84,68 @@ public class TableauApp {
     }
 
     /**
-     * A helper function which return the number of rows in a specific columns in a two dimensional array.
-     * @param t a two-dimensional array.
+     * A helper function which return the number of rows in a specific columns.
+     *
+     * @param t        a two-dimensional array.
      * @param indexCol the specific column.
      * @return int which is the number of rows in the specific column.
-     * */
+     */
     private static int getNumberOfRow(int[][] t, int indexCol) {
         int maxExpectedRows = t.length;
         int i;
-        for (i=1; i<maxExpectedRows; i++) {
+        for (i = 1; i < maxExpectedRows; i++) {
             try {
                 int dummy = t[indexCol][i];
             } catch (ArrayIndexOutOfBoundsException ex) {
                 break;
             }
         }
-        // System.out.println("The number of rows in column: " + indexCol + " is: " + i);
         return i;
     }
 
 
     /**
-     * A method that returns true if from left to right in any row, the integers are increasing, otherwise false.
+     * A method that returns true if the integers are increasing.
+     *
      * @param t a two-dimensional array.
-     * @return returns true if from left to right in any row, the integers are increasing, otherwise false.
-     * */
+     * @return returns true if integers are increasing.
+     */
     public static boolean rowValuesIncrease(int[][] t) {
-        int totalColumn = t[0].length; // assume the first row has the max column.
-        for (int col=0; col<totalColumn; col++) {
-            for (int row=0; row<getNumberOfRow(t, col)-1; row++) {
+        int numberOfColumn = t[0].length;
+        for (int col = 0; col < numberOfColumn; col++) {
+            for (int row = 0; row < getNumberOfRow(t, col) - 1; row++) {
                 try {
-                    int current = t[row][col];
-                    int next = t[row+1][col];
-                    if (current>next) {
+                    int current = t[col][row];
+                    int next = t[col][row+1];
+                    if (next <= current) {
+                        System.out.println("row: " + row + "  col: " + col);
+                        System.out.println("current: " + current + "\n" + "next: " + next);
+                        return false;
+                    }
+                } catch (ArrayIndexOutOfBoundsException ex) {
+                    break;
+                }
+            }
+        }
+        return true;
+    }
+
+
+    /**
+     * A method that returns true if from top to bottom in any column,
+     * the integers are increasing, otherwise false.
+     *
+     * @param t a two-dimensional array.
+     * @return true if from top to bottom in any column,
+     * the integers are increasing, otherwise false.
+     */
+    public static boolean columnValuesIncrease(int[][] t) {
+        for (int[] rows : t) {
+            for (int i = 0; i < rows.length - 1; i++) {
+                try {
+                    int current = rows[i];
+                    int next = rows[i + 1];
+                    if (next < current) {
                         return false;
                     }
                 } catch (ArrayIndexOutOfBoundsException ex) {
@@ -124,6 +153,32 @@ public class TableauApp {
                 }
             }
         }
+        return true;
+    }
+
+    /**
+     * A method that returns true
+     * if the set of integers used is {1, 2, . . . , n}
+     * where n is the number of cells, otherwise false.
+     *
+     * @param t a two-dimensional array.
+     * @return true if it is.
+     */
+    public static boolean isSetOf1toN(int[][] t) {
+        Set<Integer> myIntSet = new HashSet<Integer>();
+        int max = t[0][0];
+        for (int[] rows : t) {
+            for (int col : rows) {
+                myIntSet.add(Integer.valueOf(col));
+                if (max <= col) {
+                    max = col;
+                }
+            }
+        }
+        if (myIntSet.size() != max) {
+            return false;
+        }
+
         return true;
     }
 
