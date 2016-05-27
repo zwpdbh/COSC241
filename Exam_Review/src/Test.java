059d5b510356cb456afdb38ecf70d8f4c05c73d5
zwpdbh <hyperion_z@outlook.com>
2016-05-27 22:12:49 +1200

    <> quickSort finished.

--- Exam_Review/src/Test.java
+++ Exam_Review/src/Test.java
@@ -2,10 +2,18 @@
  * Created by zw on 5/26/16.
  */
 import recursion.Recursion;
-
+import search_and_sorting.Sorting;
 public class Test {
     public static void main(String[] args) {
-
+        // test recursion method contain
         System.out.println(Recursion.contains("bcabaebdefg", "aeb"));
+
+        // test quickSort method
+        Integer[] testArray = {2, 8, 7, 1, 5, 3, 6, 4};
+        Sorting.quickSort(testArray, 0, testArray.length-1);
+        for (Integer each: testArray) {
+            System.out.print(each + " ");
+        }
+        System.out.println();
     }
 }
--- Exam_Review/src/search_and_sorting/Sorting.java
+++ Exam_Review/src/search_and_sorting/Sorting.java
@@ -22,7 +22,59 @@ public class Sorting {
         }
     }
 
-    
+    /**
+     * insertion sort
+     * */
+    public static void insertionSort(Comparable[] data) {
+        for (int index = 1; index < data.length; index++) {
+            Comparable key = data[index];
+            int position = index;
+
+            // shift the larger element to right
+            while (position > 0 && data[position - 1].compareTo(key) > 0) {
+                data[position] = data[position - 1];
+                position--;
+            }
+            data[position] = key;
+        }
+
+    }
+
+    /**
+     * bubble sort
+     * */
+    public static void bubbleSort(Comparable[] data) {
+        for (int count = data.length - 1; count >= 0; count--) {
+            for (int scan = 0; scan < count; scan++) {
+                if (data[scan].compareTo(scan + 1)>0) {
+                    swap(data, scan, scan+1);
+                }
+            }
+        }
+    }
+
+    /**
+     * quick sort
+     * */
+    public static void quickSort(Comparable[] data, int left, int right) {
+        if (left < right) {
+            int pivot = partition(data, left, right);
+            quickSort(data, left, pivot-1);
+            quickSort(data, pivot+1, right);
+        }
+    }
+
+    private static int partition(Comparable[] data, int p, int r) {
+        int i = p - 1;
+        for (int j = p; j<r; j++) {
+            if (data[j].compareTo(data[r]) <=0) {
+                i++;
+                swap(data, i, j);
+            }
+        }
+        swap(data, i+1, r);
+        return i + 1;
+    }
 
     private static void swap(Comparable[] data, int index1, int index2) {
         Comparable tmp = data[index1];
